a9c948eacccf1516429399292aa93753e78c3594
HunchedBack <justinhjh6603@gmail.com>
2023-01-20 03:42:31 +0800

    first commit

---
+++ main-app/src/p6questions.js
@@ -0,0 +1,272 @@
+export const p6questions = [
+    {
+        text: "Express 12 tenths as a decimal.",
+        options: [
+            {id:0, text: "0.012", isCorrect: false},
+            {id:1, text: "0.12", isCorrect: false},
+            {id:2, text: "1.2", isCorrect: false},
+            {id:3, text: "12.0", isCorrect: false},
+        ]
+    },
+    {
+        text: "Round 51 872 to the nearest thousand.",
+        options: [
+            {id:0, text: "50 000", isCorrect: false},
+            {id:1, text: "51 000", isCorrect: false},
+            {id:2, text: "51 900", isCorrect: false},
+            {id:3, text: "52 000", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+    {
+        text: "",
+        options: [
+            {id:0, text: "", isCorrect: false},
+            {id:1, text: "", isCorrect: false},
+            {id:2, text: "", isCorrect: false},
+            {id:3, text: "", isCorrect: false},
+        ]
+    },
+]
